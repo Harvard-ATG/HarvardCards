3594a85e2df7914cce8544a052f6f94569962592
jazahn <jcleveng@fas.harvard.edu>
2013-06-03 14:39:11 -0400

    first commit

---
+++ harvardcards/settings/googleappengine.py
@@ -0,0 +1,10 @@
+# Google App Engine Settinsg
+from harvardcards.settings.common import *
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'google.appengine.ext.django.backends.rdbms',
+#        'INSTANCE': 'dev-harvardcards:instance1',
+#        'NAME': 'dev-harvardcards',
+#    }
+#}
