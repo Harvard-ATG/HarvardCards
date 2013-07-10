2320b42a733c76d7280413883945fe6e22be05d9
jazahn <jcleveng@fas.harvard.edu>
2013-07-10 17:31:49 -0400

    adding form data collection for collections

--- harvardcards/apps/flash/views.py
+++ harvardcards/apps/flash/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.core.context_processors import csrf
+
+from models import Collection
+from forms import CollectionForm
 
 def index(request):
-    #return HttpResponse("Hello Werld. This is the Harmony Index.")
+    #return HttpResponse("Hello Werld. This is the HarvardCards Index.")
     return render(request, 'index.html')
     
 def splash(request):
@@ -11,4 +16,20 @@ def main(request):
     return render(request, 'main.html')
     
 def create(request):
-    return render(request, 'collections/create.html')
+    # this is only POSTed when creating -- editing will be done on the fly
+    if request.method == 'POST':
+#        for key in request.POST:
+#            value = request.POST[key]
+#            message += ' '+key+'=>'+value
+        f = CollectionForm(request.POST)
+        collection = f.save()
+        
+        # get collection id
+        
+        # use collection_id to create appropriate fields
+        
+        
+        return redirect(index)
+    else:
+        return render(request, 'collections/create.html')
+    
