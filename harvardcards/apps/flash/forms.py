4c630f32cf08c8cd523d673974ab4b6b40b56a28
jazahn <jcleveng@fas.harvard.edu>
2013-07-19 17:16:25 -0400

    adding fieldform for use in saving collection / card fields

--- harvardcards/apps/flash/forms.py
+++ harvardcards/apps/flash/forms.py
@@ -1,7 +1,21 @@
 from django.forms import ModelForm
-from models import Collection
+from django.forms import MultipleChoiceField
+from models import Collection, Field
+from django import forms
+import logging
 
 class CollectionForm(ModelForm):
+    # this is wrong, putting this here implies that I want the field_data to not validate if the fields dont already exist
+    #field_data = ModelMultipleChoiceField(queryset=Field.objects.all())    
+    # what I want is a list, one that will run through and validate all incoming objects in the list as valid fields
+    # this may be a formset field?
+    #field_data = MultipleChoiceField()
+    
     class Meta:
         model = Collection
         fields = ['title', 'description']
+        
+class FieldForm(ModelForm):
+    class Meta:
+        model = Field
+        fields = ['label', 'field_type']
