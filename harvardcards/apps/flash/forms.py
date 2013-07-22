be62a1e5af079b551fe28eb595ebf1a3c65bbedc
jazahn <jcleveng@fas.harvard.edu>
2013-07-22 04:30:45 -0400

    removing comments

--- harvardcards/apps/flash/forms.py
+++ harvardcards/apps/flash/forms.py
@@ -5,12 +5,6 @@ from django import forms
 import logging
 
 class CollectionForm(ModelForm):
-    # this is wrong, putting this here implies that I want the field_data to not validate if the fields dont already exist
-    #field_data = ModelMultipleChoiceField(queryset=Field.objects.all())    
-    # what I want is a list, one that will run through and validate all incoming objects in the list as valid fields
-    # this may be a formset field?
-    #field_data = MultipleChoiceField()
-    
     class Meta:
         model = Collection
         fields = ['title', 'description']
