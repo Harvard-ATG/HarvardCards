8b09abc1c1e3cdff2caeaf1ecd5bb14303491c37
jazahn <jcleveng@fas.harvard.edu>
2013-07-25 03:30:42 -0400

    view changed to allow editing of decks

--- harvardcards/apps/flash/views.py
+++ harvardcards/apps/flash/views.py
@@ -86,21 +86,26 @@ def delete(request):
     
     return HttpResponse('{"success": %s}' % returnValue, mimetype="application/json")
     
+# 
 def createDeck(request, deck_id=None):
     if request.method == 'POST':
         if 'deck_id' in request.POST:
             # then it's an edit
             deck = Deck.objects.get(id=request.POST['deck_id'])
+            deck_id = request.POST['deck_id']
             deckForm = DeckForm(request.POST, instance=deck)
+            collection_id = deck.collection.id
         else:
             # then it's a new one
             deckForm = DeckForm(request.POST)
+            collection_id = request.POST['collection_id']
             
         deckForm = DeckForm(request.POST)
         if deckForm.is_valid():
             deck = deckForm.save(commit=False)
-            collection_id = request.POST['collection_id']
             deck.collection = Collection.objects.get(id=collection_id)
+            if deck_id:
+                deck.id = deck_id
             deck.save()
             if deck:
                 return HttpResponse('{"success": true}', mimetype="application/json")
