b10bec125b25c2c73ecc86fbe36d518c1494f11b
jazahn <jcleveng@fas.harvard.edu>
2013-07-28 12:47:37 -0400

    links to cdns fixed

--- harvardcards/static/js/main.js
+++ harvardcards/static/js/main.js
@@ -1,13 +1,14 @@
 requirejs.config({
 	paths: {
 		'jquery' : 'http://code.jquery.com/jquery-1.10.1.min.js',
-		'bootstrap' : '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js'
+		'lodash' : '//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash',
+		'bootstrap' : '//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min'
 	},
 	shim: {
-		'jquery' : '$'
+		'jquery' : '$',
+		'lodash' : {
+			exports: '_'
+		}
 	}
 });
 
-require(['jquery'], function($) {
-	//$ points to jQuery
-});
