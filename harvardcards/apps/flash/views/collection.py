0df1c00d3ed0e06132c20a2b3665b1cde70cbea5
jazahn <jcleveng@fas.harvard.edu>
2013-08-01 12:20:47 -0400

    fixes #2, keeps existing fields -- edits them as opposed to deleting and repopulating

--- harvardcards/apps/flash/views/collection.py
+++ harvardcards/apps/flash/views/collection.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ViewDoesNotExist
 from django.utils import simplejson as json
 
 from django.forms.formsets import formset_factory
-from harvardcards.apps.flash.models import Collection, Deck
+from harvardcards.apps.flash.models import Collection, Deck, Field
 from harvardcards.apps.flash.forms import CollectionForm, FieldForm, DeckForm
 
 def index(request):
@@ -28,30 +28,49 @@ def create(request, collection_id=None):
         if 'collection_id' in request.POST:
             collection = Collection.objects.get(id=request.POST['collection_id'])
             collectionForm = CollectionForm(request.POST, instance=collection)
-            # this needs to be changed, can't just delete them
-            # TODO: github issue #2
-            collection.field_set.all().delete()
+            
         else:
             collectionForm = CollectionForm(request.POST)
         
         if collectionForm.is_valid():
             collection = collectionForm.save()
 
-        
             # create the formset from the base fieldform
             #FieldFormSet = formset_factory(FieldForm)
             # decode json
             data = json.loads(request.POST['field_data'])            
             #return HttpResponse(repr(data))
             
+            # is it an edit?
+            # get all ids from data
+            editList = []
+            for d in data:
+                if 'id' in d:
+                    editList.append(d['id']);
+            if len(editList):
+                # then run through all ids in the db
+                # if they are not in edit list, delete them
+                existingFields = Field.objects.filter(collection=collection.id)
+                for ef in existingFields:
+                    if ef.id not in editList:
+                        ef.delete()
+                
+
             # run through field_data
             for d in data:
-                fieldForm = FieldForm(d)
-                f = fieldForm.save(commit=False) 
+                if 'id' in d:
+                    field = Field.objects.get(id=d['id'])
+                    fieldForm = FieldForm(d, instance=field)
+                else:
+                    fieldForm = FieldForm(d)
+                
+                f = fieldForm.save(commit=False)
                 # this is how relationships have to be done -- forms cannot handle this
                 # so you have to do it directly at the model
                 f.collection = collection
                 f.save()
+                
+                
 
             return redirect(index)
         else:
