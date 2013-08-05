8361d7369a56bbe413fd127f9203c1e173fa9c99
jazahn <jcleveng@fas.harvard.edu>
2013-08-05 03:18:20 -0400

    adding deck throught this interface now works -- forwards you along to the edit page after a title is chosen...

--- harvardcards/static/js/models/Deck.js
+++ harvardcards/static/js/models/Deck.js
@@ -1,28 +1,39 @@
 define(['jquery', 'lodash', 'bootstrap'], function($, _, bootstrap){
-	var Deck = function(deck_id){
+	var Deck = function(collection_id, deck_id){
 		var id = deck_id;
+		var collection_id = collection_id;
 	}
 	
 	_.extend(Deck.prototype, {
-		collectionNav: function(){
+		collectionNav: function(collection_id){
 			$('.collection-home-link').click(function(){
 				window.location = "/index";
 			});
+			$('.collection-link[data-id='+collection_id+']').addClass("active-collection");
 			
 		},
 		
 		newDeckButton: function(){
-			
+			$('#add-deck-button').click(function(){
+				window.location = "/deck/";				
+			});
 		},
-		newDeckInit: function(){
+		// this only happens if there is no deck id associated
+		newDeckInit: function(collection_id){
 			// create an input
+			if(this.id != ''){
+				// setup the input
+				this.setupEditable(collection_id);
+			}
+			// else do nothing
 		},
 		setupTitle: function(){
 			// click title
 		},
-		setupEditable: function(){
-			// make the div editable
-			$('.deck-text').editable(function(value, settings){
+
+		// make the div editable
+		setupEditable: function(collection_id){
+			$('.deck-title-text').editable(function(value, settings){
 				return value;
 			}, { 
 				tooltip   : 'Click to edit...',
@@ -31,14 +42,15 @@ define(['jquery', 'lodash', 'bootstrap'], function($, _, bootstrap){
 				onblur: 'submit',
 				// save it when done editing...
 				callback: function(value, settings){
+					//var collection_id = this.collection_id;
 					// send ajax to save it
 					$.ajax({
 						type: 'POST',
 						url: '/deck/create/',
-						data: {title: $(this).html(), deck_id: $(this).data('deck_id')},
+						data: {title: $(this).html(), collection_id: collection_id},
 						success: function(data, statusText){
 							if(data.success){
-					
+								window.location = "/deck/"+data.id;
 							} else {
 								alert("Error: "+data.message)
 							}
@@ -52,6 +64,8 @@ define(['jquery', 'lodash', 'bootstrap'], function($, _, bootstrap){
 			
 				}
 			});
+			$('.deck-title-text').trigger('click');
+			
 		}
 	
 	
