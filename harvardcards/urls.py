a8073b589c805578f6244e7a3aba8d5ccbb09dee
jazahn <jcleveng@fas.harvard.edu>
2013-08-07 00:39:46 -0400

    collection/fields url added

--- harvardcards/urls.py
+++ harvardcards/urls.py
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     url(r'^deck/create/$', 'harvardcards.apps.flash.views.deck.create', name='createDeck'),
     url(r'^deck/delete/$', 'harvardcards.apps.flash.views.deck.delete', name='deleteDeck'),
     url(r'^deck/$', 'harvardcards.apps.flash.views.deck.index', name='deckIndex'),
-    url(r'^deck/(?P<deck_id>\d+)$', 'harvardcards.apps.flash.views.deck.index', name='deckIndex')
+    url(r'^deck/(?P<deck_id>\d+)$', 'harvardcards.apps.flash.views.deck.index', name='deckIndex'),
+    url(r'^collection/fields/$', 'harvardcards.apps.flash.views.collection.fields', name='fields')
     
     #url(r'^login/$', 'django_openid_auth.views.login_begin', name='openid-login'),
 	#url(r'^login-complete/$', 'django_openid_auth.views.login_complete', name='openid-complete'),
