44e34a7e9b06040c9e6d14e2df7e02d630459646
jazahn <jcleveng@fas.harvard.edu>
2013-08-12 03:42:26 -0400

    distinguishing between edit template and view template

--- harvardcards/static/js/models/Field.js
+++ harvardcards/static/js/models/Field.js
@@ -1,9 +1,10 @@
 define(['jquery', 'lodash', 'bootstrap'], function($, _, bootstrap){
-	var Field = function(label, field_type, field_id, display){
+	var Field = function(label, field_type, field_id, display, value){
 		this.label = label;
 		this.field_type = field_type;
 		this.field_id = field_id;
 		this.display = display;
+		this.value = value;
 		var display_class = "display";
 		if(display){
 			display_class = "display";
@@ -12,11 +13,15 @@ define(['jquery', 'lodash', 'bootstrap'], function($, _, bootstrap){
 		}
 		
 		if(field_type == 'T'){
-			this.template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+'</span> '
+			this.edit_template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+'</span> '
 				+ '<input class="text-field-template" data-id="'+field_id+'" type="text"/></div> ';
+			this.view_template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+':</span> '
+				+ '<span class="text-field-template" type="text">'+this.value+'</span></div> ';
 		} else if(field_type == 'I'){
-			this.template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+'</span> '
+			this.edit_template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+'</span> '
 				+ '<input class="img-field-template" data-id="'+field_id+'" type="text"/></div> ';
+			this.view_template = '<div class="field-template '+display_class+'"><span class="mylabel">'+label+'</span> '
+				+ '<img class="img-field-template" src="'+this.value+'"/></div> ';
 		} else {
 			this.template = 'Not a valid type';
 		}
