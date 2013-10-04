567d89f963dbf877e50e3f6102f29840c3303e78
Muhammad Hassan Tahir <muhammadtahir@college.harvard.edu>
2013-10-04 12:34:03 -0400

    first test works

--- harvardcards/apps/flash/tests.py
+++ harvardcards/apps/flash/tests.py
@@ -5,20 +5,28 @@ from django.core.exceptions import ViewDoesNotExist
 
 from django.utils import simplejson as json
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
 from django.forms.formsets import formset_factory
 from harvardcards.apps.flash.models import Collection, Deck, Field
 from harvardcards.apps.flash.forms import CollectionForm, FieldForm, DeckForm
-from django.test.client import RequestFactory
+from django.test.client import RequestFactory, Client
 from harvardcards.apps.flash.views.collection import *
 
 class CollectionTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
+        self.client = Client()
+
+    def test_index(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        collections = Collection.objects.all()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
     def test_collection(self):
         title = "title of card"
         des = "nothing in description"
-        collection = create({"title": title, "description": des}, collection_id=1)
-        delt = delete({"id":1})
-        TestCase.assertEqual(HTTPResponse('{"success": "true"}', mimetype="application/json"), delt)
+
+        TestCase.assertEqual()
