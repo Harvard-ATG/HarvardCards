be45771fa0719036b17a9fdf6f06df96a252dd0c
jazahn <jcleveng@fas.harvard.edu>
2013-11-01 02:21:03 -0400

    a collection model for the ui

---
+++ harvardcards/static/js/models/Collection.js
@@ -0,0 +1,13 @@
+define('models/Collection', ['lodash'], function(_){
+	var Collection = function(collection_id){
+		this.title;
+	}
+	
+	_.extend(Collection.prototype, {
+
+
+	});
+	
+	return Collection;
+	
+});
