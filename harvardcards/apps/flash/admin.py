b818b3dad10cd880f53ae0d8da88be86256d33b5
Hassan <muhammadtahir@college.harvard.edu>
2013-11-20 13:32:33 -0500

    fixed add course

--- harvardcards/apps/flash/admin.py
+++ harvardcards/apps/flash/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from harvardcards.apps.flash.models import Collection, Field, Card, User, Users_Collections, Deck, Decks_Cards, Cards_Fields
 
+class CardsInLine(admin.TabularInline):
+    model = Cards_Fields
+    extra = 1
+
+class CardAdmin(admin.ModelAdmin):
+    inlines=(CardsInLine,)
+
 admin.site.register(Collection)
 admin.site.register(Field)
-admin.site.register(Card)
+admin.site.register(Card, CardAdmin)
 admin.site.register(User)
 admin.site.register(Users_Collections)
 admin.site.register(Deck)
