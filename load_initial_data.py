7ad77e65997662a12b8a4f2f336b2de300d883c1
Hassan <muhammadtahir@college.harvard.edu>
2013-11-29 22:56:41 -0500

    before load data pull request

--- load_initial_data.py
+++ load_initial_data.py
@@ -1,15 +1,37 @@
-import os
+import os, sys
+
+if len(sys.argv) == 2:
+    if sys.argv[1]=='create':
+        os.system("python initial_data.py")
+    else:
+        print "Invalid Command."
+        sys.exit()
+
+elif len(sys.argv) == 3:
+    json_file = sys.argv[2]
+    if not json_file.endswith('.json'):
+        print "Invalid filename."
+        sys.exit()
+    os.system("python initial_data.py %s" %json_file)
+
 
 def loaddata(file):
     if os.path.splitext(file)[1] == '.json':
         print file
         os.system("python manage.py loaddata %s" % file)
 
+
+path_fixtures = 'harvardcards/apps/flash/fixtures/'
+
+json_files = []
+
+for file in os.listdir(path_fixtures):
+    if file.endswith('.json'):
+        json_files.append(file)
+json_files =  sorted(json_files, key= lambda x: x.split("_")[0])
+
 files = []
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_collection.json'))
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_decks.json'))
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_cards.json'))
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_fields.json'))
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_cards_fields.json'))
-files.append(os.path.join(os.path.dirname(__file__),'harvardcards/apps/flash/fixtures/','initial_data_decks_cards.json'))
+for i in range(0, len(json_files)):
+    files.append(os.path.join(os.path.dirname(__file__),path_fixtures,json_files[i]))
+
 map(loaddata, files)
