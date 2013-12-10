f96f0058ad9fc553506d11fcc2d6568c4b71166a
Arthur Barrett <abarrett@fas.harvard.edu>
2013-12-10 04:34:35 +0000

    show deck menu on hover

--- harvardcards/settings/dev.py
+++ harvardcards/settings/dev.py
@@ -18,7 +18,7 @@ DATABASES = {
 MIDDLEWARE_CLASSES.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 INSTALLED_APPS.append('debug_toolbar')
 DEBUG_TOOLBAR_CONFIG = {
-    #'INTERCEPT_REDIRECTS': True,
+    'INTERCEPT_REDIRECTS': False,
     'SHOW_TOOLBAR_CALLBACK': lambda x: True,
     'HIDE_DJANGO_SQL': False,
     'TAG': 'div',
