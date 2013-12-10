c866412cd9d119287af25d214a68192cc832fdad
Arthur Barrett <abarrett@fas.harvard.edu>
2013-12-10 14:56:11 -0500

    view was missing full list of fields, so added all the card fields

--- harvardcards/apps/flash/views/deck.py
+++ harvardcards/apps/flash/views/deck.py
@@ -16,36 +16,35 @@ def test2(request):
     return render(request, "decks/test2.html")
 
 def index(request, deck_id=None):
-    collections = Collection.objects.all()  
-    if not deck_id:
-        # then it's a create
-        # and if it's a create, the request should be a post with the collection_id in it
-        if 'collection_id' in request.POST:
-            collection_id = request.POST['collection_id']
-            current_collection = Collection.objects.get(id=collection_id)
-        else:
-            raise ViewDoesNotExist
-        deck = None
-        cards = None
-    else:
-        deck = Deck.objects.get(id=deck_id)
-        current_collection = Collection.objects.get(id=deck.collection.id)
-        #deck_cards = Deck.objects.card_set.all()
-        #decks_cards = Deck.objects.filter(deck=deck)
-        deck_cards = Decks_Cards.objects.filter(deck=deck)
-        cards = []
-        for dc in deck_cards:
-            card_dict = {}
-            card_dict['card_id'] = dc.card.id
-            cf = dc.card.cards_fields_set.all()[0]
-            card_dict['first_label'] = cf.field.label
-            card_dict['first_value'] = cf.value
-            cards.append(card_dict)
+	collections = Collection.objects.all()
+	deck = Deck.objects.get(id=deck_id)
+	deck_cards = Decks_Cards.objects.filter(deck=deck).order_by('sort_order').prefetch_related('card')
+
+	cards = []
+	for dcard in deck_cards:
+		card_fields = {'show':[],'reveal':[]} 
+		for cfield in dcard.card.cards_fields_set.all().order_by('sort_order'):
+			bucket = 'show'
+			if cfield.field.display:
+				bucket = 'reveal'
+			card_fields[bucket].append({
+				'type': cfield.field.field_type,
+				'label': cfield.field.label,
+				'show_label': cfield.field.show_label,
+				'value': cfield.value,
+			})
+		cards.append({
+			'card_id': dcard.card.id,
+			'fields': card_fields
+		})
+
+	context = {
+		"collections": collections,
+		"deck": deck,
+		"cards": cards
+	}
         
-        #Decks_Cards.objects.filter(deck=deck).card.all()
-    return render(request, "deck_view.html",
-        {"collections": collections, "deck": deck, "cards": cards, 
-        "collection_id": current_collection.id, "collection": current_collection})
+	return render(request, "deck_view.html", context)
 
 # 
 def create(request, deck_id=None):
