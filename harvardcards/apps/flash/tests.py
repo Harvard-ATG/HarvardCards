2e2f22a23d161a867e11894244432cad26045673
Arthur Barrett <abarrett@fas.harvard.edu>
2014-02-24 22:25:03 +0000

    fix failing test

--- harvardcards/apps/flash/tests.py
+++ harvardcards/apps/flash/tests.py
@@ -32,16 +32,11 @@ class CollectionTest(TestCase):
         #self.assertTemplateUsed(response, 'index.html')
 
     def test_collection_get(self):
-        url = reverse('createCollection')
+        url = reverse('collection_id')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-        #TODO why is this failing?
-	    #This is failing because we don't have a separate template for add a course now
-
-        #self.assertTemplateUsed(response, 'collections/create.html')
 
     def test_collection_create(self):
-
         url = reverse('collectionCreate')
         post_data = {'title':'foobar', 'card_template':'1'}
 
@@ -52,8 +47,7 @@ class CollectionTest(TestCase):
         self.assertEqual(len_collections_before + 1, len_collections_after)
 
     def test_collection_form(self):
-        post_data = {'field_data': '[{"field_type":"T","label":"","sort_order":0,"display":true},{"field_type":"I","label":"","sort_order":1,"display":true},{"field_type":"T","label":"","sort_order":2,"display":false}]', 'csrfmiddlewaretoken': '38vLxTwts8C4pUcFqoOgQAq3eXgAdpro', 'field_type1': 'text', 'description': 'lots of math', 'title': 'math 454'}
-        # testing the form
+        post_data = {'title':'foobar', 'card_template':'1'}
         form = CollectionForm(post_data)
         self.assertEqual(form.is_valid(), True)
         form1 = CollectionForm({})
