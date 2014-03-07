0bb4f151a766f90fcb7667cb4db3b53c2f728bf4
Hassan Tahir <muhammadtahir@college.harvard.edu>
2014-03-07 15:18:22 -0500

    fixed minor issues with image upload

--- harvardcards/apps/flash/api/card.py
+++ harvardcards/apps/flash/api/card.py
@@ -5,35 +5,9 @@ from django.utils import simplejson as json
 from harvardcards.apps.flash.models import Collection, Deck, Card, Cards_Fields, Field
 from harvardcards.apps.flash import services, queries, utils
 from django.db import models
-import time
-import os
 
-def handle_uploaded_file(file):
-    curr_dir = os.getcwd()
-    folder_name = 'uploads'
-    parent_dir = os.path.abspath(os.path.join(curr_dir, 'harvardcards', 'apps', 'flash'))
-    parent_dir1 = os.path.abspath(os.path.join(parent_dir, folder_name))
-    if not os.path.exists(parent_dir1):
-        os.chdir(parent_dir)
-        os.mkdir(folder_name)
-        os.chdir(curr_dir)
-    dirfmt = "%4d-%02d-%02d"
-    dir_name = dirfmt % time.localtime()[0:3]
-    path = os.path.abspath(os.path.join(parent_dir1, dir_name))
-    if not os.path.exists(path):
-        os.chdir(parent_dir1)
-        os.mkdir(dir_name)
-        os.chdir(curr_dir)
-    file_name = file.name
-    full_path = os.path.join(path, file_name)
-    dest = open(os.path.join(path, file_name), 'wb+')
-    if file.multiple_chunks:
-        for c in file.chunks():
-            dest.write(c)
-    else:
-        dest.write(file.read())
-    dest.close()
-    return os.path.join('\media', dir_name, file_name)
+
+
 
 @require_http_methods(["POST"])
 def create(request):
@@ -46,7 +20,7 @@ def create(request):
         if field_name.startswith(field_prefix):
             field_id = field_name.replace(field_prefix, '')
             if field_id.isdigit():
-                path = handle_uploaded_file(request.FILES[field_name])
+                path = services.handle_uploaded_img_file(request.FILES[field_name], deck.title, deck.collection)
                 fields.append({"field_id": int(field_id), "value": path})
 
     for field_name, field_value in request.POST.items():
--- harvardcards/apps/flash/services.py
+++ harvardcards/apps/flash/services.py
@@ -5,7 +5,9 @@ This module contains services and commands that may change the state of the syst
 from django.db import transaction
 
 from harvardcards.apps.flash.models import Collection, Deck, Card, Decks_Cards, Cards_Fields, Field
-from harvardcards.apps.flash import utils 
+from harvardcards.apps.flash import utils
+import win32con, win32api,os
+from harvardcards.settings.common import MEDIA_ROOT
 
 def delete_collection(collection_id):
     """Deletes a collection and returns true on success, false otherwise."""
@@ -16,7 +18,15 @@ def delete_collection(collection_id):
 
 def delete_deck(deck_id):
     """Deletes a deck and returns true on success, false otherwise."""
-    Deck.objects.get(id=deck_id).delete()
+    deck = Deck.objects.get(id=deck_id)
+    folder_name = str(deck.collection) + '_' + deck.title
+    folder_path = os.path.abspath(os.path.join(MEDIA_ROOT, folder_name))
+    if os.path.exists(folder_path):
+        #to force deletion of a file set it to normal
+        win32api.SetFileAttributes(folder_path, win32con.FILE_ATTRIBUTE_NORMAL)
+        os.remove(folder_path)
+    deck.delete()
+
     if not Deck.objects.filter(id=deck_id):
         return True
     return False
@@ -27,7 +37,37 @@ def delete_card(card_id):
     if not Deck.objects.filter(id=card_id):
         return True
     return False
-    
+
+def handle_uploaded_img_file(file, deck, collection):
+    curr_dir = os.getcwd()
+    folder_name = 'uploads'
+    parent_dir = os.path.abspath(os.path.join(curr_dir, 'harvardcards', 'apps', 'flash'))
+    parent_dir1 = os.path.abspath(os.path.join(parent_dir, folder_name))
+    if not os.path.exists(parent_dir1):
+        os.chdir(parent_dir)
+        os.mkdir(folder_name)
+        os.chdir(curr_dir)
+    #dirfmt = "%4d-%02d-%02d"
+    dir_name = str(collection) +'_'+deck
+    path = os.path.abspath(os.path.join(parent_dir1, dir_name))
+    if not os.path.exists(path):
+        os.chdir(parent_dir1)
+        os.mkdir(dir_name)
+        os.chdir(curr_dir)
+    file_name = file.name
+    full_path = os.path.join(path, file_name)
+    if os.path.exists(full_path):
+        file_name = '1'+file_name
+        full_path = os.path.join(path, file_name)
+    dest = open(full_path, 'wb+')
+    if file.multiple_chunks:
+        for c in file.chunks():
+            dest.write(c)
+    else:
+        dest.write(file.read())
+    dest.close()
+    return os.path.join('\media', dir_name, file_name)
+
 def handle_uploaded_deck_file(collection_id, deck_title, uploaded_file):
     """Handles an uploaded deck."""
     collection = Collection.objects.get(id=collection_id)
