94da8308b9c51de5d1fa8556a3280765c3a443ea
jazahn <jcleveng@fas.harvard.edu>
2014-07-17 08:51:58 -0400

    fixing a broken unit test, success required data to be sent

--- harvardcards/static/js/views/CardForm.js
+++ harvardcards/static/js/views/CardForm.js
@@ -1,95 +1,97 @@
 define(['lodash', 'jquery', 'jquery.form'], function(_, $, $form) {
 
-    // This module sets up the card form used for adding new cards to a deck.
-    // Uses the jquery.form plugin to AJAXify the html form.
-    var CardForm = function(config) {
-        if(!config || !config['formEl'] || !config['formMessageEl']) {
-			console.log("card form", config);
-            throw new Error("invalid or missing CardForm config");
-        }
-        this.formEl = $(config.formEl);
-        this.formMessageEl = $(config.formMessageEl);
+	// This module sets up the card form used for adding new cards to a deck.
+	// Uses the jquery.form plugin to AJAXify the html form.
+	var CardForm = function(config) {
+		if(!config || !config['formEl'] || !config['formMessageEl']) {
+			//console.log("card form", config);
+			throw new Error("invalid or missing CardForm config");
+		}
+		this.formEl = $(config.formEl);
+		this.formMessageEl = $(config.formMessageEl);
 		this.error = $.proxy(this.error, this);
 		this.success = $.proxy(this.success, this);
-    };
-
-    CardForm.prototype.init = function() {
-        // bind these methods to the "this" context since they
-        // will be passed to the jQuery form plugin
-        _.bindAll(this, ['beforeSubmit', 'success', 'error']);
-
-        // use the jQuery.form plugin to AJAXify the form
-        this.formEl.ajaxForm({
-            resetForm: false,
-            beforeSubmit: this.beforeSubmit,
-            error: this.error,
-            success: this.success
-        });
 	};
 
-    // Status types for showing messages (constants)
-    var MSG_INFO = CardForm.MSG_INFO = "info";
-    var MSG_SUCCESS = CardForm.MSG_SUCCESS = "success";
-    var MSG_ERROR = CardForm.MSG_ERROR = "error";
+	CardForm.prototype.init = function() {
+		// bind these methods to the "this" context since they
+		// will be passed to the jQuery form plugin
+		_.bindAll(this, ['beforeSubmit', 'success', 'error']);
 
-    CardForm.prototype.beforeSubmit = function() {
-        this.msg("Saving card...", MSG_INFO);
-        this.hideErrors();
-    };
+		// use the jQuery.form plugin to AJAXify the form
+		this.formEl.ajaxForm({
+			resetForm: false,
+			beforeSubmit: this.beforeSubmit,
+			error: this.error,
+			success: this.success
+		});
+	};
 
-    CardForm.prototype.success = function(data, statusText, xhr, formEl) {
-        console.log("success", arguments);
-        var that = this;
-        if (data.success){
-            this.msg("Card saved", MSG_SUCCESS);
-            if(data.data.card_url) {
-                window.setTimeout(this.makeRedirect(data.data.card_url), 500)
-            }
-        } else {
-            this.msg("Error saving card", MSG_ERROR);
-            $.each(data.errors, function(key, val) {
-                that.setFieldError(key, val);
-            });
-            this.showErrors();
-        }
-    };
+	// Status types for showing messages (constants)
+	var MSG_INFO = CardForm.MSG_INFO = "info";
+	var MSG_SUCCESS = CardForm.MSG_SUCCESS = "success";
+	var MSG_ERROR = CardForm.MSG_ERROR = "error";
 
-    CardForm.prototype.setFieldError = function(key, val) {
-        this.formEl.find('.field-error-'+key).html(val);
-    };
+	CardForm.prototype.beforeSubmit = function() {
+		this.msg("Saving card...", MSG_INFO);
+		this.hideErrors();
+	};
 
-    CardForm.prototype.showErrors = function() {
-        this.formEl.find('.field-error').show();
-    };
+	CardForm.prototype.success = function(data, statusText, xhr, formEl) {
+		//console.log("success", arguments);
+		var that = this;
+		if (data !== undefined && data.success){
+			this.msg("Card saved", MSG_SUCCESS);
+			if(data.data.card_url) {
+				window.setTimeout(this.makeRedirect(data.data.card_url), 500)
+			}
+		} else {
+			this.msg("Error saving card", MSG_ERROR);
+			if(data !== undefined){
+				$.each(data.errors, function(key, val) {
+					that.setFieldError(key, val);
+				});
+			}
+			this.showErrors();
+		}
+	};
 
-    CardForm.prototype.hideErrors = function() {
-        this.formEl.find('.field-error').hide();
-    };
+	CardForm.prototype.setFieldError = function(key, val) {
+		this.formEl.find('.field-error-'+key).html(val);
+	};
 
-    CardForm.prototype.makeRedirect = function(location) {
-        return function() {
-            window.location = location;
-        };
-    };
+	CardForm.prototype.showErrors = function() {
+		this.formEl.find('.field-error').show();
+	};
 
-    CardForm.prototype.error = function(xhr, textStatus, errorThrown) {
-        this.msg("Card error: " + textStatus, MSG_ERROR);
-    };
+	CardForm.prototype.hideErrors = function() {
+		this.formEl.find('.field-error').hide();
+	};
 
-    // Displays a message about the status of the form.
-    CardForm.prototype.msg = function(html, statusType) {
-        this.formMessageEl.css('display', 'block');
-        var css, css_for = {};
-        css_for[MSG_SUCCESS] = {color:"green"};
-        css_for[MSG_ERROR] = {color:"red"};
-        css_for[MSG_INFO] = {color:"black"};
+	CardForm.prototype.makeRedirect = function(location) {
+		return function() {
+			window.location = location;
+		};
+	};
 
-        css = css_for[statusType];
-        if(!css) {
-            css = css_for[MSG_INFO];
-        }
-        this.formMessageEl.css(css).html(html);
-    };
+	CardForm.prototype.error = function(xhr, textStatus, errorThrown) {
+		this.msg("Card error: " + textStatus, MSG_ERROR);
+	};
 
-    return CardForm;
+	// Displays a message about the status of the form.
+	CardForm.prototype.msg = function(html, statusType) {
+		this.formMessageEl.css('display', 'block');
+		var css, css_for = {};
+		css_for[MSG_SUCCESS] = {color:"green"};
+		css_for[MSG_ERROR] = {color:"red"};
+		css_for[MSG_INFO] = {color:"black"};
+
+		css = css_for[statusType];
+		if(!css) {
+			css = css_for[MSG_INFO];
+		}
+		this.formMessageEl.css(css).html(html);
+	};
+
+	return CardForm;
 });
