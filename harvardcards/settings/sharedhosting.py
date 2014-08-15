90fce066564b6c7fe240b1099e5acae3cea7e27c
Arthur Barrett <abarrett@fas.harvard.edu>
2014-08-15 12:04:33 -0400

    changed method of retrieving of sensitive credentials due to env not working as intended

--- harvardcards/settings/sharedhosting.py
+++ harvardcards/settings/sharedhosting.py
@@ -18,16 +18,26 @@ elif os.environ.get('SERVER_NAME') == 'sites.dev.fas.harvard.edu':
     STATIC_URL = '/~harvardcards/static/'
 	DEBUG = True
 
+
 # Configuration common to both PROD/DEV 
+CONFIG_DIR = os.path.join(ROOT_DIR, 'config')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'OPTIONS': {
-            'read_default_file': os.path.join(ROOT_DIR, 'config', 'my.cnf'),
+            'read_default_file': os.path.join(CONFIG_DIR, 'my.cnf'),
         }
     }
 }
 
-# These values should be retrieved from the environment for security reasons
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-LTI_OAUTH_CREDENTIALS = dict([tuple(os.environ.get('LTI_OAUTH_CREDENTIALS').split(':',2))])
+# These are sensitive values that should be retrieved from separate configuration files.
+# Note that these config files should *NEVER* be stored in version control.
+SECRET_KEY = None
+with open(os.path.join(CONFIG_DIR, 'django_secret.txt')) as f:
+    SECRET_KEY = f.read().strip()
+
+LTI_OAUTH_CREDENTIALS = {}
+with open(os.path.join(CONFIG_DIR, 'lti_oauth_credentials.txt')) as f:
+    oauth_key_and_secret = f.read().strip().split(':',2)
+    LTI_OAUTH_CREDENTIALS = dict([tuple(oauth_key_and_secret)])
