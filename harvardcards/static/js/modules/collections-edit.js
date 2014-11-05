d4a374eee426b251b6a95bd364877684691df0c9
Arthur Barrett <abarrett@fas.harvard.edu>
2014-11-05 16:25:42 -0500

    show sort order explicitly

--- harvardcards/static/js/modules/collections-edit.js
+++ harvardcards/static/js/modules/collections-edit.js
@@ -13,17 +13,31 @@ define(['jquery', 'jqueryui'], function($) {
 			console.log("sortable event", "args:", event, ui);
 
 			var deck_ids = []; 
-			var json_deck_ids = '';
+			var deck_order = [];
+			var deck_json = '';
 
+			// find all deck IDs as they are ordered in the DOM
 			$(event.target).find("[data-deck-id]").each(function(index, el) {
 				var id = $(el).data("deck-id");
 				deck_ids.push(id);
 			});
 
-			json_deck_ids = JSON.stringify({"deck_ids": deck_ids});
-			$("#deck_order").val(json_deck_ids);
+			// convert to a list that explicitly has the sort order 
+			deck_order = $.map(deck_ids, function(val, index) {
+				var sort_order = index + 1; // because index starts at zero
+				return {"sort_order": sort_order, "deck_id": val};
+			});
 
-			console.log("new deck order: ", deck_ids, "json:", json_deck_ids);
+			// convert to JSON format so it can be stored in a hidden input field
+			// that will be POSTed along with the rest of the form
+			deck_json = JSON.stringify({"deck": deck_order});
+
+			$("#deck_order").val(deck_json);
+
+			console.log("--> new deck order:");
+			console.log("deck_ids:", deck_ids);
+			console.log("deck_order", deck_order);
+			console.log("deck_json:", deck_json);
 		});
 	}
 
