330398a446368685b1ed8fb7bcf60fd08a38bf5f
JaZahn <jcleveng@fas.harvard.edu>
2015-01-22 14:11:17 -0500

    using localStorage to keep track of what's open / closed

--- harvardcards/static/js/modules/collections-index.js
+++ harvardcards/static/js/modules/collections-index.js
@@ -1,11 +1,29 @@
 define(['jquery', 'components/slider/CollectionSlider', 'utils/utils', 'jqueryui'], function($, CollectionSlider, utils) {
 	return {
 		initModule: function(){
+
+
             $('.courseHeader').click(function() {
-                $(this).next().toggle('slow');
+                var el = this;
+                $(this).next().toggle('slow', function(){
+                    if($(el).next().is(":visible")){
+                        var collection_id = $(el).data('collection-id');
+                        localStorage.setItem('show-collection-id'+collection_id, true);
+                    } else {
+                        localStorage.removeItem('show-collection-id'+collection_id);
+                    }
+                });
                 $(this).find('.plusminus').toggleClass("fa-plus-circle fa-minus-circle");
                 return false;
             }).next().hide();
+
+            $('.courseHeader').each(function(index){
+                var el = this;
+                var collection_id = $(el).data('collection-id');
+                if(localStorage.getItem('show-collection-id'+collection_id) == 'true'){
+                    $(el).click();
+                }
+            });
 
 		}
 	};
