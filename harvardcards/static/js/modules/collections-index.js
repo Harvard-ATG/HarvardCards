38269d0df5a261d70eb338af267bf75570e40cb8
JaZahn <jcleveng@fas.harvard.edu>
2015-01-23 13:44:00 -0500

    moving onclick function to it's own handler

--- harvardcards/static/js/modules/collections-index.js
+++ harvardcards/static/js/modules/collections-index.js
@@ -1,21 +1,21 @@
-define(['jquery', 'components/slider/CollectionSlider', 'utils/utils', 'jqueryui'], function($, CollectionSlider, utils) {
-	return {
+define(['jquery', 'utils/utils'], function($, utils) {
+	var courseAccordion = function(courseHeader){
+        var courseBody = $(courseHeader).next();
+        $(courseBody).toggle('slow', function(){
+            var collection_id = $(courseHeader).data('collection-id');
+            if($(courseBody).is(":visible")){
+                localStorage.setItem('show-collection-id'+collection_id, true);
+            } else {
+                localStorage.removeItem('show-collection-id'+collection_id);
+            }
+        });
+        $(courseHeader).find('.plusminus').toggleClass("fa-plus-circle fa-minus-circle");
+        return true;
+    }
+
+    return {
 		initModule: function(){
-
-
-            $('.courseHeader').click(function() {
-                var el = this;
-                $(this).next().toggle('slow', function(){
-                    var collection_id = $(el).data('collection-id');
-                    if($(el).next().is(":visible")){
-                        localStorage.setItem('show-collection-id'+collection_id, true);
-                    } else {
-                        localStorage.removeItem('show-collection-id'+collection_id);
-                    }
-                });
-                $(this).find('.plusminus').toggleClass("fa-plus-circle fa-minus-circle");
-                return false;
-            }).next().hide();
+            $('.courseHeader').click(function(){ return courseAccordion(this); });
 
             $('.courseHeader').each(function(index){
                 var el = this;
