b9917ca81a9be38fc2dca26ba2c10d21a82b4487
Arthur Barrett <abarrett@edx.org>
2015-02-09 17:46:39 -0500

    Fixed typo.

--- harvardcards/apps/flash/forms.py
+++ harvardcards/apps/flash/forms.py
@@ -175,6 +175,6 @@ class CardEditForm(forms.Form):
 
     def _check_file_errors(self):
         for f in self.files:
-            is_valid_type = services.valid_uploaded_file(self.files[f], 'I')
+            is_valid_type = services.valid_image_file_type(self.files[f])
             if not is_valid_type:
                 self.errors[f] = "File image type is not supported. Must be: .jpg, .png, or .gif"
