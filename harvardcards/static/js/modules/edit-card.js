cae90e121cc780ca7c69162e0084f46debac94f8
Arthur Barrett <abarrett@edx.org>
2015-02-11 11:03:35 -0500

    Removed console.log debug statements.

--- harvardcards/static/js/modules/edit-card.js
+++ harvardcards/static/js/modules/edit-card.js
@@ -2,14 +2,12 @@ define(['jquery', 'views/CardForm'], function($, CardForm) {
 
     function setup_file_url_switch() {
         $('select[data-switch]').each(function(idx, el) {
-        console.log("setup", idx, el);
             var url_selector = $(el).data("switch-url");
             var file_selector = $(el).data("switch-file");
             $(el).on("change", function() {
                 var source_type = $(el).val();
                 $('input[name='+file_selector+']')[source_type=='U'?'hide':'show']();
                 $('input[name='+url_selector+']')[source_type=='U'?'show':'hide']();
-                console.log("change", idx, el, source_type, "this", this);
             });
         });
     }
