eaaae13420a52b8ee08e04736f9398d439442b61
Arthur Barrett <abarrett@fas.harvard.edu>
2015-06-19 17:26:38 -0400

    Changed from simple_tag to filter.

--- harvardcards/apps/flash/templatetags/media_store_helpers.py
+++ harvardcards/apps/flash/templatetags/media_store_helpers.py
@@ -3,8 +3,10 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.simple_tag
-def get_media_url(object_path, object_id):
+@register.filter(name='as_media_url')
+def as_media_url(value, arg):
+    object_id = value
+    object_path = arg
     if settings.MEDIA_STORE_BACKEND == 's3':
         url = "http//s3.amazonaws.com/%s/%s/%s" % (settings.AWS_S3_BUCKET, object_path, object_id)
     else:
