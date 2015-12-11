06a3a5c78aa0d65bf68f3c1a036ffe8dc5589857
JaZahn <jcleveng@fas.harvard.edu>
2015-12-11 09:57:42 -0500

    TEMPLATE_* configs will be removed in favor of the TEMPLATE.OPTIONS dict

--- harvardcards/settings/base.py
+++ harvardcards/settings/base.py
@@ -6,7 +6,6 @@ from os import path
 
 # Django settings for harvardcards project.
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -68,19 +67,40 @@ STATICFILES_DIRS = [
 	path.join(ROOT_DIR, 'static'),
 ]
 
+TEMPLATES = [
+	{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'OPTIONS': {
+			'debug': True,
+            'context_processors': [
+                	"django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages"
+            ],
+			'DIRS': [
+				path.join(ROOT_DIR, 'templates'),
+			    path.join(ROOT_DIR, 'flash', 'templates'),
+			    path.join(ROOT_DIR, 'jasmine', 'templates'),
+			],
+			'loaders': [
+			    'django.template.loaders.filesystem.Loader',
+			    'django.template.loaders.app_directories.Loader',
+				# 'django.template.loaders.eggs.Loader',
+			]
+		}
+    }
+]
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = [
@@ -96,27 +116,6 @@ ROOT_URLCONF = 'harvardcards.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'harvardcards.wsgi.application'
-
-TEMPLATE_DIRS = [
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    path.join(ROOT_DIR, 'templates'),
-]
-
-TEMPLATE_DIRS.extend([
-    path.join(ROOT_DIR, 'flash', 'templates'),
-    path.join(ROOT_DIR, 'jasmine', 'templates'),
-])
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.request',
-)
 
 INSTALLED_APPS = [
     'django.contrib.auth',
